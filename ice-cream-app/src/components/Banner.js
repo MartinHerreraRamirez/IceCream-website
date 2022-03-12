e63a47044fb0489a0bc4f2940d9b1d9e78c4edfd
Marivi <mmondolo@hotmail.com>
2022-03-12 10:49:53 +0100

    Añade la barra de navegación

---
+++ ice-cream-app/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <>
+    <div>
+        <h1>ENVÍOS GRATUITOS A PARTIR DE 2.000$ </h1>
+    </div>
+    </>
+  );
+}
+
+export default Banner;
