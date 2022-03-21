09b84de3cb7bd2898a34b7764ecf39c09103d876
Marivi <mmondolo@hotmail.com>
2022-03-21 20:30:28 +0100

    Cambia logo y color del banner

--- ice-cream-app/src/components/Banner.js
+++ ice-cream-app/src/components/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Banner() {
   return (
-    <section class="text-black py-2 bg-teal-100 m-0">
+    <section class="text-white py-0.5 bg-blue-700 m-0">
       <div class="max-w-screen-xl py-0 mx-auto lg:h-12 lg:items-center lg:flex">
         <div class="max-w-3xl mx-auto text-center">
           <p class="max-w-xl mx-auto sm:leading-relaxed sm:text-s">
--- ice-cream-app/src/components/Nav.jsx
+++ ice-cream-app/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 /* import Button from './Button'; */
-import logo from "./assets/logo.png";
+import lunaice from "./assets/lunaice.png"
 
 const Nav = () => {
   let Links = [
@@ -14,7 +14,7 @@ const Nav = () => {
     <div className="shadow-md w-full top-15 left-0">
       <div className="flex items-center justify-between bg-white py-1 md:px-5 px-7">
         <div>
-          <img src={logo} alt="logo" class="w-1/5 m-0" />
+          <img src={lunaice} alt="logo" class="w-1/5 m-0" />
         </div>
 
         <div
@@ -26,7 +26,7 @@ const Nav = () => {
 
         <ul
           className={`md:flex md:items-center sx:mt-14 md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-32" : "top-[-490px]"
+            open ? "top-28" : "top-[-490px]"
           }`}
         >
           {Links.map((link) => (
