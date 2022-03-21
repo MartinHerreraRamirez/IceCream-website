c7c21d4e0a31314b1de585601ee1d6eb965c0db7
Marcos <marcostonda@gmail.com>
2022-03-21 16:16:18 -0300

    SE CREA COMPONENTE Boton

--- ice-cream-app/src/App.js
+++ ice-cream-app/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <button className="font-serif font-medium text-sm text-zinc-50 hover:text-black p-3 bg-auto rounded border-4 border-sky-500 hover:border-red-800 hover:bg-cyan-600 bg-indigo-700">HOLA</button>
-    </div>
+    <>
+    <Boton></ Boton>
+    </>
   );
 }
 
