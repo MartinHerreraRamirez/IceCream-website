aeebf3549f6629740754ea542dfe76d8bc026fd3
Marivi <mmondolo@hotmail.com>
2022-03-23 00:13:05 +0100

    Añade título e iguala tamaño imagenes

--- ice-cream-app/src/components/Tarjetas.js
+++ ice-cream-app/src/components/Tarjetas.js
@@ -6,14 +6,20 @@ import varios from "../assets/varios.jpg"
 
 const Tarjeta = () => {
   return (
+    <>
+    <div className='mt-4 mb-10 text-center text-2xl'>
+      <h2>
+        Para todos los gustos
+      </h2>
+    </div>
     <div class="flex justify-items-center grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-rows-4">
       <div class="rounded-lg shadow-lg bg-white w-72">
         <a href="#!">
-          <img class="rounded-t-lg" src={fresas} alt="fresas" />
+          <img class="rounded-t-lg h-52 object-cover" src={fresas} alt="fresas" />
         </a>
         <div class="p-6">
-          <h5 class="text-gray-900 text-xl font-medium mb-2">
-            Título de la tarjeta
+          <h5 class="text-gray-900 text-xl text-center font-medium mb-2">
+            Para Vegan@s
           </h5>
           <p class="text-blue-700 text-base mb-4">
             Texto que explica el contenido de esta tarjeta. Como por ejemplo
@@ -21,19 +27,19 @@ const Tarjeta = () => {
           </p>
           <button
             type="button"
-            class="inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="ml-7 px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Botón
+            ¡Compra ahora!
           </button>
         </div>
       </div>
       <div class="rounded-lg shadow-lg bg-white w-72 md-flex">
         <a href="#!">
-          <img class="rounded-t-lg objet" src={linda} alt="galgo tomando helado" />
+          <img class="rounded-t-lg h-52 w-72 object-cover" src={linda} alt="galgo tomando helado" />
         </a>
         <div class="p-6">
-          <h5 class="text-gray-900 text-xl font-medium mb-2">
-            Título de la tarjeta
+          <h5 class="text-gray-900 text-xl text-center font-medium mb-2">
+            Para Mascotas
           </h5>
           <p class="text-blue-700 text-base mb-4">
             Texto que explica el contenido de esta tarjeta. Como por ejemplo
@@ -41,19 +47,19 @@ const Tarjeta = () => {
           </p>
           <button
             type="button"
-            class="inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="ml-7 inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Botón
+            ¡Compra ahora!
           </button>
         </div>
       </div>
       <div class="rounded-lg shadow-lg bg-white w-72">
         <a href="#!">
-          <img class="rounded-t-lg" src={crema} alt="crema" />
+          <img class="rounded-t-lg h-52 object-cover" src={crema} alt="crema" />
         </a>
         <div class="p-6">
-          <h5 class="text-gray-900 text-xl font-medium mb-2">
-            Título de la tarjeta
+          <h5 class="text-gray-900 text-xl text-center font-medium mb-2">
+            Para Celíac@s
           </h5>
           <p class="text-blue-700 text-base mb-4">
             Texto que explica el contenido de esta tarjeta. Como por ejemplo
@@ -61,19 +67,19 @@ const Tarjeta = () => {
           </p>
           <button
             type="button"
-            class="inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="ml-7 inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Botón
+            ¡Compra ahora!
           </button>
         </div>
       </div>
       <div class="rounded-lg shadow-lg bg-white w-72">
         <a href="#!">
-          <img class="rounded-t-lg" src={varios} alt="varios sabores" />
+          <img class="rounded-t-lg h-52 object-cover" src={varios} alt="varios sabores" />
         </a>
         <div class="p-6">
-          <h5 class="text-gray-900 text-xl font-medium mb-2">
-            Título de la tarjeta
+          <h5 class="text-gray-900 text-xl text-center font-medium mb-2">
+            Para Gordit@s
           </h5>
           <p class="text-blue-700 text-base mb-4">
             Texto que explica el contenido de esta tarjeta. Como por ejemplo
@@ -81,13 +87,14 @@ const Tarjeta = () => {
           </p>
           <button
             type="button"
-            class="inline-block  px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="ml-7 inline-block px-10 py-2.5 bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
-            Botón
+            ¡Compra ahora!
           </button>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
