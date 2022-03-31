61f303d041c0823f6a41f462d1fcdef02244e889
Marcos <marcostonda@gmail.com>
2022-03-31 17:40:25 -0300

    ventana modal mas sencilla

--- ice-cream-app/src/App.js
+++ ice-cream-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Modal from "./components/Modal";
+import { useModalHookState } from "./components/useModalHookState";
+import Pic1 from "./components/pictures/pic01.jpg";
 
-function App() {
+const App = () => {
+  const [modalIsOpen, openModal, closeModal] = useModalHookState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <button
+        className="px-6 m-10 font-sans text-white bg-blue-600 border-2 rounded border-cyan-500"
+        onClick={openModal}
+      >
+        MODAL
+      </button>
+      <Modal modalIsOpen={modalIsOpen} closeModal={closeModal}>
+        <h2 className="items-center justify-center mb-4 text-4xl text-center text-black">
+          Producto A
+        </h2>
+        <div>
+        <img
+          src={Pic1}
+          className="object-contain border-2 border-black rounded-md sm:m-2 md:m-4 lg:m-6 shrink"
+          alt="Helado"
+        ></img>
+        </div>
+      </Modal>
+    </div> 
   );
-}
+};
 
 export default App;
