c419214d6871a209271071231da134db7369157b
Marcos <marcostonda@gmail.com>
2022-04-23 16:00:37 -0300

    se agrega index.js

--- ice-cream-app/src/App.js
+++ ice-cream-app/src/App.js
@@ -1,1 +1,31 @@
+import Modal from "./components/Modal";
+import { useModalHookState } from "./components/useModalHookState";
+import Pic1 from "./components/pictures/pic01.jpg";
 
+const App = () => {
+  const [modalIsOpen, openModal, closeModal] = useModalHookState(false);
+  return (
+    <div>
+      <button
+        className="px-6 m-10 font-sans text-white bg-blue-600 border-2 rounded border-cyan-500"
+        onClick={openModal}
+      >
+        MODAL
+      </button>
+      <Modal modalIsOpen={modalIsOpen} closeModal={closeModal}>
+        <h2 className="items-center justify-center mb-4 text-4xl text-center text-black">
+          Producto A
+        </h2>
+        <div>
+        <img
+          src={Pic1}
+          className="object-contain border-2 border-black rounded-md sm:m-2 md:m-4 lg:m-6 shrink"
+          alt="Helado"
+        ></img>
+        </div>
+      </Modal>
+    </div> 
+  );
+};
+
+export default App;
